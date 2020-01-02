d04fdba8528d76afa0c3ab2aca610e5bb3396eeb
mibpl <3181064+mibpl@users.noreply.github.com>
2020-01-01 17:40:05 -0800

    npm, react skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ display: "none" }}>
+      <p>
+        Hello world
+      </p>
+    </div>
+  );
+}
+
+export default App;

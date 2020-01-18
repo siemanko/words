3564fc827fffd7fe129c955d124eac28dae2eb54
mibpl <3181064+mibpl@users.noreply.github.com>
2020-01-18 13:33:18 -0800

    Navbar -> class, reformat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Board, BoardNotifications} from  './Board.js';
-import {RecommendBox, RecommendBoxNotifications} from  './RecommendBox.js';
+import { Board, BoardNotifications } from './Board.js';
+import { RecommendBox, RecommendBoxNotifications } from './RecommendBox.js';
 import './App.css';
 import natural from 'natural';
 
@@ -13,7 +13,7 @@ function App() {
       <NavBar />
       <AboutModal />
       <SettingsModal />
-      <GamePanel ref={(gamePanelComponent) => {window.gamePanelComponent = gamePanelComponent}} />
+      <GamePanel ref={(gamePanelComponent) => { window.gamePanelComponent = gamePanelComponent }} />
     </div>
   );
 }
@@ -55,7 +55,7 @@ function SettingsModal() {
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        
+
         <div className="modal-body">
           <form>
             <div className="form-group">
@@ -64,7 +64,7 @@ function SettingsModal() {
                 <small>Do you sometimes accidentally reveal a word because of your clumsy laptop touchpad? This option might be for you!.</small>
               </label>
               <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="double-click"/>
+                <input className="form-check-input" type="checkbox" value="" id="double-click" />
                 <label className="form-check-label" htmlFor="double-click">
                   require double click / tap to reveal a word
                 </label>
@@ -88,7 +88,7 @@ function SettingsModal() {
                 <small>In <tt>Give clues</tt> view we will display a list of suggested clues for the player to chose from. It is still up to them whether to use the suggestion or not.</small>
               </label>
               <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="human-cluemaster-hints"/>
+                <input className="form-check-input" type="checkbox" value="" id="human-cluemaster-hints" />
                 <label className="form-check-label" htmlFor="human-cluemaster-hints">
                   help me come up with clues
                 </label>
@@ -98,7 +98,7 @@ function SettingsModal() {
             <div className="form-group">
               <label className="text-justify">
                 Use only popular words<br />
-                <small>The model can produce clues based on pretty large dictionary of words - about 30k - this allows it to sometimes find an original and awesome hint 
+                <small>The model can produce clues based on pretty large dictionary of words - about 30k - this allows it to sometimes find an original and awesome hint
                   for a board where human might think that all hope is lost. However, this comes at a price - the clues can sometimes be unusual, offensive, hard to understrand or even google!
                   If this is too much for you consider the option below. </small>
               </label>
@@ -133,7 +133,7 @@ function SettingsModal() {
                 <small>Displays information about the automatic clue suggestion model, e.g. which words is the clue related to or what is its score.</small>
               </label>
               <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="debug-enable" disabled/>
+                <input className="form-check-input" type="checkbox" value="" id="debug-enable" disabled />
                 <label className="form-check-label" htmlFor="debug-enable">
                   I want to know everything! <i>(coming soon!)</i>
                 </label>
@@ -141,7 +141,7 @@ function SettingsModal() {
             </div>
           </form>
         </div>
-       
+
       </div>
     </div>
   </div>
@@ -149,118 +149,124 @@ function SettingsModal() {
 }
 
 class GamePanel extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            game: null
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      game: null
+    };
+  }
 
-    render() {
-        const game = this.state.game;
+  render() {
+    const game = this.state.game;
 
-        if (game === null) {
-            return <div />;
-        } else if (game.error !== null) {
-            return <p class="lead"> {game.error}</p>;
-        } else {
-            return (
-                <div>
-                  <div>
-                    <RecommendBoxNotifications game={game} />
-                    <BoardNotifications game={game} />
-                  </div>
-                  <div className="container-fluid h-100">
-                    <div className="row h-100">
-                      <RecommendBox game={game} />
-                      <Board game={game} />
-                    </div>
-                  </div>
-                </div>
-            );
-        }
+    if (game === null) {
+      return <div />;
+    } else if (game.error !== null) {
+      return <p class="lead"> {game.error}</p>;
+    } else {
+      return (
+        <div>
+          <div>
+            <RecommendBoxNotifications game={game} />
+            <BoardNotifications game={game} />
+          </div>
+          <div className="container-fluid h-100">
+            <div className="row h-100">
+              <RecommendBox game={game} />
+              <Board game={game} />
+            </div>
+          </div>
+        </div>
+      );
     }
+  }
 }
 
-function NavBar() {
-  return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light" id="main-navbar">
-      <span className="navbar-brand">Words</span>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="menu">
-        {/* navigation buttons */}
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          
-          {/* game modes  */}
-          <li className="nav-item dropdown">
-            <a className="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img src="icons/detective.svg" style={{ height: '2rem' }} />&nbsp;Game Mode
-            </a>
-            <div className="dropdown-menu">
-              <a className="dropdown-item" href="#" id="guess">
-                <img src="icons/guess.svg" style={{ height: '2rem' }} />&nbsp;Guess
-              </a>
-              <a className="dropdown-item" href="#" id="cluemaster">
-                <img src="icons/detective.svg" style={{ height: '2rem' }} />&nbsp;Give&nbsp;Clues
-              </a>
-              <a className="dropdown-item" href="#" id="ai-cluemaster">
-                <img src="icons/ai.svg" style={{ height: '2rem' }} />&nbsp;AI&nbsp;Clues
-              </a>
-            </div>
-          </li>
-          
-          {/* settings */}
-          <li className="nav-item"><a href='#' id="about" className="nav-link" data-toggle="modal" data-target="#settings-modal">
-            <img src="icons/settings.svg" style={{ height: "2rem" }} />&nbsp;Settings
-            </a>
-          </li>
-          {/* about */}
-          <li className="nav-item"><a href='#' id="about" className="nav-link" data-toggle="modal" data-target="#about-modal">
-            <img src="icons/info.svg" style={{ height: "2rem" }} />&nbsp;About
-            </a>
-          </li>
-          {/* reset  */}
-          <li className="nav-item"><a href='#' id="reset" className="nav-link">
-            <img src="icons/reset.svg" style={{ height: "2rem" }} />&nbsp;Reset
-            </a>
-          </li>
-        </ul>
-        <div className="nav-text">
-          <span
-            style={{ color: 'red' }}
-            data-toggle="tooltip"
-            data-placement="bottom"
-            title="Number of red tiles that weren't discovered yet.">
-            Red:&nbsp;<span id="red-left"></span>
-          </span>
-          &nbsp;&nbsp;
-            <span
-            style={{ color: 'blue' }}
-            data-toggle="tooltip"
-            data-placement="bottom"
-            align="right"
-            title="Number of blue tiles that weren't discovered yet.">
-            Blue:&nbsp;<span id="blue-left"></span>
-          </span>
-          &nbsp;
-          &nbsp;
-        </div>
-        {/* seed input */}
-        <form className="form-inline my-2 my-lg-0">
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">seed</span>
-            </div>
-            <input className="form-control" type="text" className="form-control" id="seed"
-              placeholder="seed" data-toggle="tooltip" data-placement="bottom" title="Type this on a different device to get the same game" />
-          </div>
-        </form>
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-      </div >
-    </nav >
-  );
+  render() {
+    return (
+      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light" id="main-navbar">
+        <span className="navbar-brand">Words</span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="menu">
+          {/* navigation buttons */}
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+
+            {/* game modes  */}
+            <li className="nav-item dropdown">
+              <a className="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="icons/detective.svg" style={{ height: '2rem' }} />&nbsp;Game Mode
+            </a>
+              <div className="dropdown-menu">
+                <a className="dropdown-item" href="#" id="guess">
+                  <img src="icons/guess.svg" style={{ height: '2rem' }} />&nbsp;Guess
+              </a>
+                <a className="dropdown-item" href="#" id="cluemaster">
+                  <img src="icons/detective.svg" style={{ height: '2rem' }} />&nbsp;Give&nbsp;Clues
+              </a>
+                <a className="dropdown-item" href="#" id="ai-cluemaster">
+                  <img src="icons/ai.svg" style={{ height: '2rem' }} />&nbsp;AI&nbsp;Clues
+              </a>
+              </div>
+            </li>
+
+            {/* settings */}
+            <li className="nav-item"><a href='#' id="about" className="nav-link" data-toggle="modal" data-target="#settings-modal">
+              <img src="icons/settings.svg" style={{ height: "2rem" }} />&nbsp;Settings
+            </a>
+            </li>
+            {/* about */}
+            <li className="nav-item"><a href='#' id="about" className="nav-link" data-toggle="modal" data-target="#about-modal">
+              <img src="icons/info.svg" style={{ height: "2rem" }} />&nbsp;About
+            </a>
+            </li>
+            {/* reset  */}
+            <li className="nav-item"><a href='#' id="reset" className="nav-link">
+              <img src="icons/reset.svg" style={{ height: "2rem" }} />&nbsp;Reset
+            </a>
+            </li>
+          </ul>
+          <div className="nav-text">
+            <span
+              style={{ color: 'red' }}
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="Number of red tiles that weren't discovered yet.">
+              Red:&nbsp;<span id="red-left"></span>
+            </span>
+            &nbsp;&nbsp;
+            <span
+              style={{ color: 'blue' }}
+              data-toggle="tooltip"
+              data-placement="bottom"
+              align="right"
+              title="Number of blue tiles that weren't discovered yet.">
+              Blue:&nbsp;<span id="blue-left"></span>
+            </span>
+            &nbsp;
+            &nbsp;
+        </div>
+          {/* seed input */}
+          <form className="form-inline my-2 my-lg-0">
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="basic-addon1">seed</span>
+              </div>
+              <input className="form-control" type="text" className="form-control" id="seed"
+                placeholder="seed" data-toggle="tooltip" data-placement="bottom" title="Type this on a different device to get the same game" />
+            </div>
+          </form>
+
+        </div >
+      </nav >
+    );
+  }
 }
 
 export default App;
